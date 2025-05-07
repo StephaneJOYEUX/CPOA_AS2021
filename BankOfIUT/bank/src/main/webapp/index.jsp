678491257d2d68a176c5f3ee1a38a60a1044185b
= <stephane.joyeux@univ-lorraine.fr>
2025-05-07 10:28:24 +0200

    TEST

--- BankOfIUT/bank/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World - ICI BANK OF IUT !</h2>
-</body>
-</html>
--- td1/src/main/java/com/iut/as/interfaces/ISommeProduit.java
+++
@@ -1,9 +0,0 @@
-package com.iut.as.interfaces;
-
-public interface ISommeProduit {
-
-	int somme(int a, int b);
-
-	int produit(int a, int b);
-
-}
